a7f22d6ccc3375225af5d0fe95414a2f5ce17d59
Terence1991 <cplterencehawes@gmail.com>
2018-10-17 17:08:09 +0000

    skeleton of routes added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router  = express.Router();
+const PORT = 8000;
 
 module.exports = (knex) => {
 
@@ -16,3 +17,57 @@ module.exports = (knex) => {
 
   return router;
 }
+
+
+
+app.get("/maps", (req, res) => {
+//see list of available maps created by users.
+
+
+});
+
+
+app.get("/maps/:id" , (req, res) => {
+  //-See one map in detail with option to edit
+  //Click markers to get more information about locations
+
+});
+
+app.get("/maps/edit/:id", (req, res) => {
+  //Display map edit form
+
+});
+
+app.get('/user/:id', (req, res) => {
+//display user profile
+
+})
+
+app.get("/maps/new", (req, res) => {
+//Display map edit form
+
+});
+
+
+app.get("/login/:id" , (req, res) => {
+//   -Req.session.user id =req.parama.id
+// -redirect(/maps
+
+});
+
+app.post('/maps', (req, res) => {
+  
+  //add a new map, redirect to /maps
+
+});
+
+
+app.put('/map/id', (req, res) => {
+  // Updates map redirect to /maps:id
+})
+
+
+
+
+
+
