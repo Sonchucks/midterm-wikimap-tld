fedd0562d4199d410024132946118c45ba9411ee
Louis Riehl <louisriehl@gmail.com>
2018-10-17 20:45:02 +0000

    reformatted routes, added new migration and seed

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,10 +1,13 @@
 exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
+      return knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+    })
+    .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({username: 'Alice', email: 'alice@gmail.com', password: '214$!@5697124$!@$'}),
+        knex('users').insert({username: 'Bob', email: 'bob@gmail.com', password: '847518hadsf!@#51#!@'}),
+        knex('users').insert({username: 'Charlie', email: 'charlie@gmail.com', password: '5325#%1#23$!@#%'})
       ]);
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,6 @@
 const express = require('express');
 const router  = express.Router();
 const PORT = 8000;
-const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session')
-
-router.use(cookieSession({
-  name: 'session',
-  keys: ['userID']
-}))
 
 module.exports = (knex) => {
 
