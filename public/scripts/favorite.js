38b5f7efae61a4fe2ab0d8f8898af74de9735378
Sonchucks <son.hyun.uk@hotmail.com>
2018-10-20 19:54:33 +0000

    Merging layout

--- public/scripts/favorite.js
+++ public/scripts/favorite.js
@@ -1,23 +1,20 @@
 $(function() {
     console.log('ready');
-$('#favorite-frm').on('submit', function(event){
-event.preventDefault()
-console.log('on submit');
+  $('#favorite-frm').on('submit', function(event){
+    event.preventDefault();
+    console.log('on submit');
 
-const mapID = $(this).find('input[name=map_ID]').val();
-const options = {url: `/maps/${mapID}/favorites`, method: 'post', dataType: 'json', data: {mapID: mapID}};
+    const mapID = $(this).find('input[name=map_ID]').val();
+    const options = {url: `/maps/${mapID}/favorites`, method: 'post', dataType: 'json', data: {mapID: mapID}};
 
-$.ajax(options)
-.done(response => {
- console.log(response);
-})
-.fail(error => {
-  console.log("Error: ", error);
+    $.ajax(options)
+    .done(response => {
+     console.log(response);
+     $('#favorite-confirm').slideDown('slow').delay(1500).slideUp('slow');
+    })
+    .fail(error => {
+      console.log("Error: ", error);
+    });
+
+  });
 });
-
-  })
-})
-
-
-
-
